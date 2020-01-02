dc4464387e47dbd0279db373a55552676206d482
Dmitry Novik <dmitry.novik@gmail.com>
2020-01-02 15:05:59 +1100

    indentation

--- restart-service-replica.ps1
+++ restart-service-replica.ps1
@@ -1,8 +1,6 @@
 Param (
-    [Parameter(Mandatory=$true)] [uri] $ApplicationName
+    [Parameter(Mandatory=$true)] [uri[]] $ApplicationNames
 )
-
-Write-Host "Restarting Service Fabric Application: $ApplicationName"
 
 try {
     Test-ServiceFabricClusterConnection | Out-Null
@@ -14,27 +12,33 @@ catch {
 $nodes = Get-ServiceFabricNode -StatusFilter Up
 $nodes | ForEach-Object {
     $nodeName = $_.NodeName
-    $replicas = Get-ServiceFabricDeployedReplica -NodeName $nodeName -ApplicationName $ApplicationName
-    Write-Host "=== Processing Node: $nodeName" -ForegroundColor Yellow
+    Write-Host "Processing node: $nodeName" -ForegroundColor Yellow
 
-    $replicas | ForEach-Object {
-		$partitionId = $_.partitionId
-        $instanceId = $_.instanceId
-        $serviceManifestName = $_.ServiceManifestName
-        $codePackageName = $_.CodePackageName
+    foreach ($applicationName in $ApplicationNames) {
 
-        # Remove the replica from the Node:
-        Write-Host "Removing $ApplicationName replica on $nodeName (partitionId: $partitionId, instanceId: $instanceId)"      
-        $success = Remove-ServiceFabricReplica -NodeName $nodeName -PartitionId $partitionId -ReplicaOrInstanceId $instanceId -CommandCompletionMode Invalid
-        if ($success) {
-            Write-Host "Successfully removed the replica of $ApplicationName on $nodeName" -ForegroundColor Green
-        }
+        $replicas = Get-ServiceFabricDeployedReplica -NodeName $nodeName -ApplicationName $applicationName
+        Write-Host "  Restarting the service fabric application: $applicationName" -ForegroundColor Magenta
 
-        # Restart the process on the Node:
-        Write-Host "Restarting $ApplicationName process on $nodeName (serviceManifestName: $serviceManifestName, codePackageName: $codePackageName)"
-        $success = Restart-ServiceFabricDeployedCodePackage -NodeName $nodeName -ApplicationName $ApplicationName -CodePackageName $codePackageName -ServiceManifestName $serviceManifestName -CommandCompletionMode Invalid
-        if ($success) {
-            Write-Host "Successfully restarted the process of $ApplicationName on $nodeName" -ForegroundColor Green
+        $replicas | ForEach-Object {
+            $partitionId = $_.partitionId
+            $instanceId = $_.instanceId
+            $serviceManifestName = $_.ServiceManifestName
+            $codePackageName = $_.CodePackageName
+
+            # Remove the replica from the Node:
+            Write-Host "    Removing $applicationName replica on $nodeName (partitionId: $partitionId, instanceId: $instanceId)"      
+            $success = Remove-ServiceFabricReplica -NodeName $nodeName -PartitionId $partitionId -ReplicaOrInstanceId $instanceId -CommandCompletionMode Invalid
+            if ($success) {
+                Write-Host "      Successfully removed the replica of $applicationName on $nodeName" -ForegroundColor Green
+            }
+
+            # Restart the process on the Node:
+            Write-Host "    Restarting $applicationName process on $nodeName (serviceManifestName: $serviceManifestName, codePackageName: $codePackageName)"
+            $success = Restart-ServiceFabricDeployedCodePackage -NodeName $nodeName -ApplicationName $applicationName -CodePackageName $codePackageName -ServiceManifestName $serviceManifestName -CommandCompletionMode Invalid
+            if ($success) {
+                Write-Host "      Successfully restarted the process of $applicationName on $nodeName" -ForegroundColor Green
+            }
         }
     }
 }
+    
