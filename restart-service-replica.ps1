e607ada0c11ef936cfab2deea67111226f3656ba
Dmitry Novik <dmitry.novik@gmail.com>
2020-01-02 14:41:59 +1100

    init

---
+++ restart-service-replica.ps1
@@ -0,0 +1,39 @@
+Param (
+    [Parameter(Mandatory=$true)] [uri] $ApplicationName,
+    $CommandCompletionMode = "Verify"
+)
+
+Write-Host "Restarting Service Fabric Application: $ApplicationName, CommandCompletionMode: $CommandCompletionMode"
+
+try {
+    Test-ServiceFabricClusterConnection | Out-Null
+}
+catch {
+    throw "Active connection to Service Fabric cluster required"
+}
+
+$nodes = Get-ServiceFabricNode -StatusFilter Up
+$nodes | ForEach-Object {
+    $nodeName = $_.NodeName
+    $replicas = Get-ServiceFabricDeployedReplica -NodeName $nodeName -ApplicationName $ApplicationName
+    $replicas | ForEach-Object {
+		$partitionId = $_.partitionId
+        $instanceId = $_.instanceId
+        $serviceManifestName = $_.ServiceManifestName
+        $codePackageName = $_.CodePackageName
+
+        # Remove the replica from the Node:
+        Write-Host "Removing $ApplicationName replica on $nodeName (partitionId: $partitionId, instanceId: $instanceId)"      
+        $success = Remove-ServiceFabricReplica -NodeName $nodeName -PartitionId $partitionId -ReplicaOrInstanceId $instanceId -CommandCompletionMode $CommandCompletionMode
+        if ($success) {
+            Write-Host "Successfully removed the replica of $ApplicationName on $nodeName" -ForegroundColor Green
+        }
+
+        # Restart the process on the Node:
+        Write-Host "Restarting $ApplicationName process on $nodeName (serviceManifestName: $serviceManifestName, codePackageName: $codePackageName)"
+        $success = Restart-ServiceFabricDeployedCodePackage -NodeName $nodeName -ApplicationName $ApplicationName -CodePackageName $codePackageName -ServiceManifestName $serviceManifestName -CommandCompletionMode Invalid
+        if ($success) {
+            Write-Host "Successfully restarted the process of $ApplicationName on $nodeName" -ForegroundColor Green
+        }
+    }
+}
