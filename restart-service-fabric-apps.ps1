563f47a8f5adbfd2da6bcce53570518b2481ddf8
Dmitry Novik <dmitry.novik@gmail.com>
2020-01-03 15:41:01 +1100

    Parallel app restart using jobs

--- restart-service-fabric-apps.ps1
+++ restart-service-fabric-apps.ps1
@@ -3,6 +3,8 @@ Param (
     [string] $ConnectionEndpoint = "localhost:19000"
 )
 
+Write-Host "Restarting apps [$Apps] on cluster: $ConnectionEndpoint" -ForegroundColor Red
+
 try {
     Write-Host "Connecting to $ConnectionEndpoint ..." -ForegroundColor Yellow
     Connect-ServiceFabricCluster -ConnectionEndpoint $ConnectionEndpoint | Out-Null
@@ -12,41 +14,65 @@ try {
 }
 
 $nodes = Get-ServiceFabricNode -StatusFilter Up
+
 $nodes | ForEach-Object {
     $nodeName = $_.NodeName
     Write-Host "Processing node: $nodeName" -ForegroundColor Yellow
 
-    foreach ($applicationName in $Apps) {
+    [int[]] $restartAppJobs =  @()
 
-        if (-Not $applicationName.ToString().StartsWith("fabric:/")) {
-            $applicationName = "fabric:/" + $applicationName;
-        }
+    $Apps | ForEach-Object {
 
-        $replicas = Get-ServiceFabricDeployedReplica -NodeName $nodeName -ApplicationName $applicationName
+        $applicationName = $_            
 
-        $replicas | ForEach-Object {
-            $partitionId = $_.partitionId
-            $instanceId = $_.instanceId
-            $serviceManifestName = $_.ServiceManifestName
-            $codePackageName = $_.CodePackageName
-            $serviceName = $_.ServiceName;
+        $restartAppJob = Start-Job -Name $nodeName+":"+$applicationName `
+            -ArgumentList $ConnectionEndpoint, $nodeName, $applicationName `
+            -ScriptBlock { 
+            param([uri] $ConnectionEndpoint, [string] $nodeName, [string] $applicationName)
 
-            Write-Host "  Restarting the service: $serviceName" -ForegroundColor Magenta
-
-            # Remove the replica from the Node:
-            Write-Host "    Removing $serviceName replica (partitionId: $partitionId, instanceId: $instanceId)"      
-            $success = Remove-ServiceFabricReplica -NodeName $nodeName -PartitionId $partitionId -ReplicaOrInstanceId $instanceId -CommandCompletionMode Verify
-            if ($success) {
-                Write-Host "      -> Successfully removed the replica of $serviceName on $nodeName" -ForegroundColor Green
+            if (-Not $applicationName.ToString().StartsWith("fabric:/")) {
+                $applicationName = "fabric:/" + $applicationName;
             }
 
-            # Restart the process on the Node:
-            Write-Host "    Restarting $serviceName process (serviceManifestName: $serviceManifestName, codePackageName: $codePackageName)"
-            $success = Restart-ServiceFabricDeployedCodePackage -NodeName $nodeName -ApplicationName $applicationName -CodePackageName $codePackageName -ServiceManifestName $serviceManifestName -CommandCompletionMode Verify
-            if ($success) {
-                Write-Host "      -> Successfully restarted the process of $serviceName" -ForegroundColor Green
+            try {
+                Write-Host "Connecting to $ConnectionEndpoint ..." -ForegroundColor Yellow
+                Connect-ServiceFabricCluster -ConnectionEndpoint $ConnectionEndpoint | Out-Null
+                Write-Host "  -> success!" -ForegroundColor Green
+            } catch {
+                throw "Could not connect to $ConnectionEndpoint"
+            }
+                    
+            $replicas = Get-ServiceFabricDeployedReplica -NodeName $nodeName -ApplicationName $applicationName
+
+            $replicas | ForEach-Object {
+                $partitionId = $_.partitionId
+                $instanceId = $_.instanceId
+                $serviceManifestName = $_.ServiceManifestName
+                $codePackageName = $_.CodePackageName
+                $serviceName = $_.ServiceName;
+
+                Write-Host "  Restarting the service: $serviceName" -ForegroundColor Magenta
+
+                # Remove the replica from the Node:
+                Write-Host "    Removing $serviceName replica (partitionId: $partitionId, instanceId: $instanceId)"      
+                $success = Remove-ServiceFabricReplica -NodeName $nodeName -PartitionId $partitionId -ReplicaOrInstanceId $instanceId -CommandCompletionMode Verify
+                if ($success) {
+                    Write-Host "      -> Successfully removed the replica of $serviceName on $nodeName" -ForegroundColor Green
+                }
+
+                # Restart the service replica's process on the Node:
+                Write-Host "    Restarting $serviceName process (serviceManifestName: $serviceManifestName, codePackageName: $codePackageName)"
+                $success = Restart-ServiceFabricDeployedCodePackage -NodeName $nodeName -ApplicationName $applicationName -CodePackageName $codePackageName -ServiceManifestName $serviceManifestName -CommandCompletionMode Verify
+                if ($success) {
+                    Write-Host "      -> Successfully restarted the process of $serviceName" -ForegroundColor Green
+                }
             }
         }
-    }
+
+        $restartAppJobs += $restartAppJob.Id
+    } # Apps | ...
+
+    Write-Host "  Node $nodeName -> waiting for parallel app restart to finish ..."
+    Wait-Job -Id $restartAppJobs
 }
     
